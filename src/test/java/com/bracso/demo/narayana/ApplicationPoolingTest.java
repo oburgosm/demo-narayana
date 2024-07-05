853a22ea409172823d78792e120a92d33440362c
oburgosm <oscarbma@ext.inditex.com>
2024-07-05 12:26:05 +0200

    refactor: Fix application after update to spring-boot 3

--- src/test/java/com/bracso/demo/narayana/ApplicationPoolingTest.java
+++ src/test/java/com/bracso/demo/narayana/ApplicationPoolingTest.java
@@ -1,6 +1,9 @@
 package com.bracso.demo.narayana;
 
+import org.apache.activemq.artemis.junit.EmbeddedActiveMQExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jms.core.JmsTemplate;
@@ -10,7 +13,11 @@ import org.springframework.transaction.support.TransactionTemplate;
 
 @SpringBootTest
 @ActiveProfiles("pooling")
+@ExtendWith(EmbeddedActiveMQExtension.class)
 class ApplicationPoolingTest {
+
+  @RegisterExtension
+  public EmbeddedActiveMQExtension server = new EmbeddedActiveMQExtension();
 
   @Autowired
   private JmsTemplate jmsTemplate;
--- src/test/java/com/bracso/demo/narayana/ApplicationTest.java
+++ src/test/java/com/bracso/demo/narayana/ApplicationTest.java
@@ -1,6 +1,8 @@
 package com.bracso.demo.narayana;
 
+import org.apache.activemq.artemis.junit.EmbeddedActiveMQExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jms.core.JmsTemplate;
@@ -8,6 +10,7 @@ import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 @SpringBootTest
+@ExtendWith(EmbeddedActiveMQExtension.class)
 class ApplicationTest {
 
   @Autowired
